f161ebc6ae4c6f5dd507a693fb85beb72ad2e26b
IoriBranford <ioribranford@gmail.com>
2023-09-30 18:50:34 -0700

    Start defining game info with title and cover art.

---
+++ src/GameInfo.ts
@@ -0,0 +1,15 @@
+export interface GameInfo {
+  title: string;
+  coverArt: string;
+}
+
+export const AllGameInfo = [
+  {
+    coverArt: "card/demonizer.png",
+    title: "Demonizer",
+  },
+  {
+    coverArt: "card/honeysoldier.jpg",
+    title: "Honey Soldier",
+  },
+];
