c279e0a25213cfd397beb7b0e26424a5ea347424
IoriBranford <ioribranford@gmail.com>
2023-09-30 20:07:10 -0700

    Improve visibility of title on game card with outline and heading.

--- src/GameCard.tsx
+++ src/GameCard.tsx
@@ -7,13 +7,13 @@ export default function GameCard({coverArt, title} : GameInfo) {
         <img src={coverArt} alt={title} style={{
             borderRadius: '16px'
         }}/>
-        <p style={{
+        <h2 style={{
             position: 'absolute',
             left: '16px',
             top: '0',
             textAlign: 'left',
             color: 'white',
-            textShadow: '2px 2px #000000',
-        }}>{title}</p>
+            textShadow: '1px 1px #000000,-1px 1px #000000,1px -1px #000000,-1px -1px #000000',
+        }}>{title}</h2>
     </div>
 }
