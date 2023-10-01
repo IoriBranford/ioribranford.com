966a8cca98ad68822cbcd6b31e8823b61cd96e72
IoriBranford <ioribranford@gmail.com>
2023-09-30 21:10:14 -0700

    Move game card element styles to css.

--- src/GameCard.tsx
+++ src/GameCard.tsx
@@ -2,20 +2,9 @@ import {GameInfo} from "./GameInfo";
 
 export default function GameCard(gameInfo : GameInfo) {
     const { title, keyArtPath, keyArtCardStyle } = gameInfo
-    return <div style={{
-        position: 'relative',
-    }}>
-        <img src={keyArtPath} alt={title} width={300} height={420} style={{
-            borderRadius: '16px',
-            ...keyArtCardStyle
-        }}/>
-        <h2 style={{
-            position: 'absolute',
-            left: '16px',
-            top: '0',
-            textAlign: 'left',
-            color: 'white',
-            textShadow: '1px 1px #000000,-1px 1px #000000,1px -1px #000000,-1px -1px #000000',
-        }}>{title}</h2>
+    return <div className="game-card">
+        <img className="game-card-image" src={keyArtPath} alt={title} width={300} height={420} 
+            style={keyArtCardStyle}/>
+        <h2 className='game-card-title'>{title}</h2>
     </div>
 }
