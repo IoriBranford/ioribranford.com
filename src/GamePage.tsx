831a5d3f41179f92ec3eec08ce708120dfc7a318
IoriBranford <ioribranford@gmail.com>
2023-10-02 17:38:27 -0700

    Try reading and rendering description.md.

--- src/GamePage.tsx
+++ src/GamePage.tsx
@@ -1,9 +1,16 @@
 import { GameInfo } from "./GameInfo";
+import { useState } from "preact/hooks";
+import Markdown from "react-markdown";
 
 export default function GamePage(gameInfo : GameInfo) {
+    const [description, setDescription] = useState("")
     const {title, keyArtPath} = gameInfo
+    fetch('/demonizer/description.md')
+    .then((response) => response.text())
+    .then((text) => setDescription(text))
     return <div className="game-page">
         <img src={keyArtPath} alt={title + " key art"}/>
-        <embed type="text/html" src="/demonizer/index.html"/>
+        <Markdown className='game-page-description'>{description}</Markdown>
+        {/* <embed type="text/html" src="/demonizer/index.html"/> */}
     </div>
 }
