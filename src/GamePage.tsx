666cedef1f19458b8cdbb5830b478397edfda1d7
IoriBranford <ioribranford@gmail.com>
2023-10-03 15:08:36 -0700

    Display logo alt text as h1.

--- src/GamePage.tsx
+++ src/GamePage.tsx
@@ -1,11 +1,11 @@
 import { GameInfo } from "./GameInfo";
 
 export default function GamePage(gameInfo : GameInfo) {
-    const {title, keyArtPath} = gameInfo
+    const {title, keyArtPath, logoPath} = gameInfo
     return <div className="game-page">
         <img src={keyArtPath} alt={title + " key art"}/>
         <div className='game-page-description'>
-            <img className='game-page-logo' src={gameInfo.logoPath} alt={gameInfo.title}/>
+            <h1><img className='game-page-logo' src={logoPath} alt={title}/></h1>
             <div dangerouslySetInnerHTML={{
                 __html: gameInfo.description
             }}/>
