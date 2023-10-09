c2657d933641a44b25f3a562f4a40431d836c813
IoriBranford <ioribranford@gmail.com>
2023-10-09 14:17:41 -0700

    Move displayed game titles to description files.

--- src/GamePage.tsx
+++ src/GamePage.tsx
@@ -1,15 +1,11 @@
 import { GameInfo } from "./GameInfo";
 
 export default function GamePage(gameInfo : GameInfo) {
-    const {title, keyArtPath, logoPath} = gameInfo
+    const {title, keyArtPath} = gameInfo
     return <div className="game-page">
         <img src={keyArtPath} alt={title + " key art"}/>
-        <div className='game-page-description'>
-            {logoPath ? <img className='game-page-logo' src={logoPath} alt={title} width='100%'/>
-            : <h1>{title}</h1>}
-            <div dangerouslySetInnerHTML={{
-                __html: gameInfo.description
-            }}/>
-        </div>
+        <div className='game-page-description' dangerouslySetInnerHTML={{
+            __html: gameInfo.description
+        }}/>
     </div>
 }
